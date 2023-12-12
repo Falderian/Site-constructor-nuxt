7014dc75c69f551f67b7dd18c02a0a6e64538556
Falderian <nikitastaselovitch@gmail.com>
2023-12-12 14:25:03 +0300

    UPD: added second variant of content page

--- components/siteConfig/contents/DynamicContent.vue
+++ components/siteConfig/contents/DynamicContent.vue
@@ -6,18 +6,9 @@
         <label>Search</label>
         <input type="search" v-model="searchQuery" />
       </div> -->
-      <div class="limit">
-        <span>Producs per page</span>
-        <input type="number" :min="limit.min" :max="limit.max" v-model="contentPerPage" @change="hanldeLimitChange" />
-      </div>
     </div>
-    <div ref="contentRef">
-      <component :is="variant" :content="currContent" />
-    </div>
-    <div class="pagination">
-      <span v-for="page in pagesNums" :class="[page === currPage && 'active', 'page-num']" @click="changePageNum(page)">
-        {{ page }}
-      </span>
+    <div class="anim-fade-in">
+      <component :is="variant" :content="products" />
     </div>
   </div>
 </template>
@@ -27,44 +18,10 @@ const { variant } = defineProps({
   variant: { type: String, required: true },
 });
 
+const { data: products } = await useAsyncData("posts", () =>
+  $fetch("https://api.escuelajs.co/api/v1/products")
+);
 
-const { data: products } = await useAsyncData('posts', () => $fetch('https://api.escuelajs.co/api/v1/products'))
-
-const contentRef = ref(null)
-const contentPerPage = ref(9)
-const currPage = ref(1)
-
-const limit = {
-  min: 5, max: 25
-}
-
-const pagesAmount = computed(() => {
-  return Math.round((products!.value as []).length / contentPerPage.value)
-})
-
-const pagesNums = computed(() => {
-  return Array.from({ length: pagesAmount.value - 1 }, (_, i) => i + 1)
-})
-
-const currContent = computed(() => {
-  return (products as Ref<[]>).value.slice(currPage.value * contentPerPage.value, (currPage.value + 1) * contentPerPage.value)
-})
-
-const toogleAnimation = () => {
-  const toggle = () => (contentRef.value! as HTMLDivElement).classList.toggle('anim-fade-in');
-  toggle();
-  setTimeout(toggle, 500);
-}
-
-const changePageNum = (num: number) => {
-  toogleAnimation()
-  currPage.value = num;
-}
-
-const hanldeLimitChange = () => {
-  toogleAnimation()
-  currPage.value = 1
-}
 </script>
 
 <script lang="ts">
@@ -83,54 +40,31 @@ export default {
   flex-direction: column;
   gap: 1rem;
 
-  .controls {
-    display: flex;
-    gap: 1rem;
+  // .controls {
+  //   display: flex;
+  //   gap: 1rem;
 
-    div {
-      display: flex;
-      align-items: center;
-      gap: 0.5rem;
+  //   div {
+  //     display: flex;
+  //     align-items: center;
+  //     gap: 0.5rem;
 
-      input {
-        background-color: #E6E6E6;
-        border: none;
-        outline: 1px solid transparent;
-        border-radius: 20px;
-        transition: 0.5s linear;
-        padding: 0.3rem 0.5rem;
+  //     input {
+  //       background-color: #e6e6e6;
+  //       border: none;
+  //       outline: 1px solid transparent;
+  //       border-radius: 20px;
+  //       transition: 0.5s linear;
+  //       padding: 0.3rem 0.5rem;
 
-        &:focus,
-        &:hover {
-          outline: 1px solid $color-text-secondary;
-        }
-      }
-    }
-  }
+  //       &:focus,
+  //       &:hover {
+  //         outline: 1px solid $color-text-secondary;
+  //       }
+  //     }
+  //   }
+  // }
 
-  .pagination {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1rem;
-    padding: 2rem;
 
-    cursor: pointer;
-
-    .active {
-      border-bottom: 1px solid $color-text-secondary;
-      color: $color-text-secondary;
-      text-decoration: overline;
-    }
-
-    .page-num {
-      transition: all .5s;
-
-      &:hover {
-        color: $color-text-secondary;
-        scale: 1.5;
-      }
-    }
-  }
 }
 </style>
--- components/siteConfig/contents/contentOne/ContentOne.vue
+++ components/siteConfig/contents/contentOne/ContentOne.vue
@@ -1,21 +1,119 @@
 <template>
-  <div class="anim-fade-in content">
-    <ContentCardOne v-for="card in content" :card="card" :key="card.title" />
+  <div class="content">
+    <div class="limit">
+      <span>Producs per page</span>
+      <input type="number" :min="limit.min" :max="limit.max" v-model="contentPerPage" @change="hanldeLimitChange" />
+    </div>
+    <div class="content__cards" ref="contentRef">
+      <ContentCard v-for="card in currContent" :card="card" :key="card.id" />
+    </div>
+    <div class="pagination">
+      <span v-for="page in pagesNums" :class="[page === currPage && 'active', 'page-num']" @click="changePageNum(page)">
+        {{ page }}
+      </span>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import ContentCardOne from './ContentCardOne.vue';
+import ContentCard from '../ContentCard.vue';
 
 const { content } = defineProps({ content: { type: Array as PropType<ICard[]>, required: true } })
+
+const contentRef = ref(null);
+const contentPerPage = ref(3);
+const currPage = ref(1);
+
+const pagesAmount = computed(() => {
+  return Math.ceil(content.length / contentPerPage.value);
+});
+
+const limit = computed(() => {
+  return { min: 3, max: content.length - 1 };
+});
+
+const pagesNums = computed(() => {
+  return Array.from({ length: pagesAmount.value }, (_, i) => i + 1);
+});
+
+const currContent = computed(() => {
+  return content.slice(
+    (currPage.value - 1) * contentPerPage.value,
+    currPage.value * contentPerPage.value
+  );
+});
+
+const toogleAnimation = () => {
+  const toggle = () =>
+    (contentRef.value! as HTMLDivElement).classList.toggle("anim-fade-in");
+  toggle();
+  setTimeout(toggle, 500);
+};
+
+const changePageNum = (num: number) => {
+  toogleAnimation();
+  currPage.value = num;
+};
+
+const hanldeLimitChange = () => {
+  toogleAnimation();
+  currPage.value = 1;
+};
+
 </script>
 
 <style scoped lang="scss">
+.pagination {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: 2rem;
+
+  cursor: pointer;
+
+  .active {
+    border-bottom: 1px solid $color-text-secondary;
+    color: $color-text-secondary;
+    text-decoration: overline;
+  }
+
+  .page-num {
+    transition: all 0.5s;
+
+    &:hover {
+      color: $color-text-secondary;
+      scale: 1.5;
+    }
+  }
+}
+
 .content {
   display: flex;
-  flex-wrap: wrap;
+  flex-direction: column;
+  align-content: space-around;
   justify-content: space-around;
-
   gap: 2rem;
+
+  .limit {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+
+    input {
+      outline: none;
+      text-align: center;
+      border: 1px solid $color-text-secondary;
+    }
+  }
+
+  &__cards {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    align-items: center;
+    min-height: 200px;
+    height: 100%;
+  }
 }
 </style>
